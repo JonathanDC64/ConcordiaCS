7918a4f629da221878c6271552b7f327f3cafedf
EpicNameBro <nintendo_jon64@hotmail.com>
2016-09-09 08:20:01 -0400

    Added a few courses

--- index.php
+++ index.php
@@ -1,8 +1,45 @@
 <html>
   <head>
-
+    <title>Concordia CS Resource</title>
+		<link href="styles.css" rel="stylesheet" />
   </head>
+
+  <header>
+    <h1>Concordia Computer Science Resource</h1>
+  </header>
+
   <body>
-    <h1>This is a test.</h1>
+    <table>
+      <tr>
+        <td>
+          Books
+        </td>
+        <td>
+          <a href="https://www.dropbox.com/sh/8mwvtj99dagmvbo/AAC8YDn2J-Y1ZN7ymbRIB3Lga?dl=0">Link</a>
+        </td>
+      </tr>
+
+      <tr>
+        <td>
+          COMP348 Principles of Programming Languages(Username: COMP348, Pass: A348C)
+        </td>
+        <td>
+          <a href="https://users.encs.concordia.ca/~c348_2/">Link</a>
+        </td>
+      </tr>
+
+      <tr>
+				<td>
+          COMP228 System Hardware(Username: COMP228, Pass: A228C)
+        </td>
+        <td>
+          <a href="https://users.encs.concordia.ca/~c228_2/">Link</a>
+        </td>
+      </tr>
+    </table>
   </body>
+
+  <footer>
+    <h6>By Jonathan Del Corpo</h6>
+  </footer>
 </html>
