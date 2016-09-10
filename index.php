3107098a84695c7f9657acda65705fa2d4ca02fd
EpicNameBro <nintendo_jon64@hotmail.com>
2016-09-10 18:54:55 -0400

    https fix

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <head>
     <title>Concordia CS Resource</title>
 		<!--Import Google Icon Font-->
-     <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+     <link href="//fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 		 <!-- Compiled and minified CSS -->
 		 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
 
