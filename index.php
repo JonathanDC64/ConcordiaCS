7ad163cb5b3600bf47e60433cc8242ae4975848d
EpicNameBro <nintendo_jon64@hotmail.com>
2016-09-10 13:41:50 -0400

    added materialize css

--- index.php
+++ index.php
@@ -1,54 +1,73 @@
 <html>
   <head>
     <title>Concordia CS Resource</title>
-		<link href="styles.css" rel="stylesheet" />
-  </head>
+		<!--Import Google Icon Font-->
+     <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+		 <!-- Compiled and minified CSS -->
+		 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
 
-  <header>
-    <h1>Concordia Computer Science Resource</h1>
-  </header>
+		 <link rel="stylesheet" href="styles.css" />
 
+     <!--Let browser know website is optimized for mobile-->
+     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	</head>
   <body>
-    <table border="1">
-      <tr>
-        <td>
-          Books
-        </td>
-        <td>
-          <a href="https://www.dropbox.com/sh/8mwvtj99dagmvbo/AAC8YDn2J-Y1ZN7ymbRIB3Lga?dl=0">Link</a>
-        </td>
-      </tr>
+		<header>
+			<nav class="red lighten-1" role="navigation">
+		    <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Concordia Computer Science Resource</a>
+		      <ul class="right hide-on-med-and-down">
+		        <li><a href="#"></a></li>
+		      </ul>
 
-      <tr>
-        <td>
-          COMP348(Username: COMP348, Pass: A348C)
-        </td>
-        <td>
-          <a href="https://users.encs.concordia.ca/~c348_2/">Link</a>
-        </td>
-      </tr>
+		      <ul id="nav-mobile" class="side-nav">
+		        <li><a href="#"></a></li>
+		      </ul>
+		      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+		    </div>
+		  </nav>
+		</header>
+		<main>
+			<div class="container">
+				<div class="section">
+			    <table class="highlight bordered">
+			      <tr>
+			        <td>
+			          <a class="red waves-effect waves-light btn" href="https://www.dropbox.com/sh/8mwvtj99dagmvbo/AAC8YDn2J-Y1ZN7ymbRIB3Lga?dl=0">Books</a>
+			        </td>
+			      </tr>
 
-      <tr>
-				<td>
-          COMP228(Username: COMP228, Pass: A228C)
-        </td>
-        <td>
-          <a href="https://users.encs.concordia.ca/~c228_2/">Link</a>
-        </td>
-      </tr>
+			      <tr>
+			        <td>
+			          <a class="red waves-effect waves-light btn"  href="https://users.encs.concordia.ca/~c348_2/">COMP348(Username: COMP348, Pass: A348C)</a>
+			        </td>
+			      </tr>
 
-			<tr>
-				<td>
-          Vectors and Matrices Mathematica
-        </td>
-        <td>
-          <a href="https://reference.wolfram.com/language/tutorial/LinearAlgebraInMathematicaOverview.html">Link</a>
-        </td>
-      </tr>
-    </table>
+			      <tr>
+			        <td>
+			          <a class="red waves-effect waves-light btn"  href="https://users.encs.concordia.ca/~c228_2/">COMP228(Username: COMP228, Pass: A228C)</a>
+			        </td>
+			      </tr>
+
+						<tr>
+			        <td>
+			          <a class="red waves-effect waves-light btn"  href="https://reference.wolfram.com/language/tutorial/LinearAlgebraInMathematicaOverview.html">Vectors and Matrices Mathematica</a>
+			        </td>
+			      </tr>
+			    </table>
+				</div>
+			</div>
+		</main>
+		<footer class="page-footer">
+      <div class="footer-copyright">
+        <div class="container">
+        © Jonathan Del Corpo
+        <a class="grey-text text-lighten-4 right" href="#!"></a>
+        </div>
+      </div>
+    </footer>
+		<!--Import jQuery before materialize.js-->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+		<!-- Compiled and minified JavaScript -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
   </body>
-
-  <footer>
-    <h6>By Jonathan Del Corpo</h6>
-  </footer>
 </html>
