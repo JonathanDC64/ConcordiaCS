9850a86eef6b95f33c53346085b9c776d0c68831
EpicNameBro <nintendo_jon64@hotmail.com>
2016-09-11 08:04:39 -0400

    fixed counter

--- counter.php
+++ counter.php
@@ -1,15 +1,15 @@
 <?php
 
+$filepath = getcwd() . "/countlog.txt";
 /* counter */
-
 //opens countlog.txt to read the number of hits
-$datei = fopen("/countlog.txt","r");
+$datei = fopen($filepath,"r");
 $count = fgets($datei,1000);
 fclose($datei);
 $count=$count + 1 ;
 
 // opens countlog.txt to change new hit number
-$datei = fopen("/countlog.txt","w");
+$datei = fopen($filepath,"w");
 fwrite($datei, $count);
 fclose($datei);
 
