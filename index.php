79d153c4d215988bbe7d9202196f3f9b21b5f983
EpicNameBro <nintendo_jon64@hotmail.com>
2016-09-11 08:38:36 -0400

    background image

--- index.php
+++ index.php
@@ -15,52 +15,52 @@
      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	</head>
   <body>
-		<header>
-			<nav class="red lighten-1" role="navigation">
-		    <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Concordia Computer Science Resource</a>
-					<ul class="right hide-on-med-and-down">
-		        <li><a class="red lighten-3 waves-effect waves-light btn"><?= $count ?> Views</a></li>
-		      </ul>
+	<header>
+		<nav class="red lighten-1" role="navigation">
+			<div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Concordia Computer Science Resource</a>
+				<ul class="right hide-on-med-and-down">
+					<li><a class="red lighten-3 waves-effect waves-light btn"><?= $count ?> Views</a></li>
+				</ul>
 
-		      <ul id="nav-mobile" class="side-nav">
-		        <li><a href="#">Navbar Link</a></li>
-		      </ul>
-		      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons"></i></a>
-		    </div>
-		  </nav>
-		</header>
-		<main>
-			<div class="container">
-				<div class="section">
-			    <table class="highlight bordered">
-			      <tr>
-			        <td>
-			          <a class="red waves-effect waves-light btn" href="https://www.dropbox.com/sh/8mwvtj99dagmvbo/AAC8YDn2J-Y1ZN7ymbRIB3Lga?dl=0">Books</a>
-			        </td>
-			      </tr>
-
-			      <tr>
-			        <td>
-			          <a class="red waves-effect waves-light btn"  href="https://users.encs.concordia.ca/~c348_2/">COMP348(Username: COMP348, Pass: A348C)</a>
-			        </td>
-			      </tr>
-
-			      <tr>
-			        <td>
-			          <a class="red waves-effect waves-light btn"  href="https://users.encs.concordia.ca/~c228_2/">COMP228(Username: COMP228, Pass: A228C)</a>
-			        </td>
-			      </tr>
-
-						<tr>
-			        <td>
-			          <a class="red waves-effect waves-light btn"  href="https://reference.wolfram.com/language/tutorial/LinearAlgebraInMathematicaOverview.html">Vectors and Matrices Mathematica</a>
-			        </td>
-			      </tr>
-			    </table>
-				</div>
+				<ul id="nav-mobile" class="side-nav">
+					<li><a href="#">Navbar Link</a></li>
+				</ul>
+			  <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons"></i></a>
 			</div>
-		</main>
-		<footer class="page-footer">
+		</nav>
+	</header>
+	<main>
+		<div class="container">
+			<div class="section">
+				<table class="highlight bordered">
+				  <tr>
+					<td>
+					  <a class="red waves-effect waves-light btn" href="https://www.dropbox.com/sh/8mwvtj99dagmvbo/AAC8YDn2J-Y1ZN7ymbRIB3Lga?dl=0">Books</a>
+					</td>
+				  </tr>
+
+				  <tr>
+					<td>
+					  <a class="red waves-effect waves-light btn"  href="https://users.encs.concordia.ca/~c348_2/">COMP348(Username: COMP348, Pass: A348C)</a>
+					</td>
+				  </tr>
+
+				  <tr>
+					<td>
+					  <a class="red waves-effect waves-light btn"  href="https://users.encs.concordia.ca/~c228_2/">COMP228(Username: COMP228, Pass: A228C)</a>
+					</td>
+				  </tr>
+
+				<tr>
+					<td>
+					  <a class="red waves-effect waves-light btn"  href="https://reference.wolfram.com/language/tutorial/LinearAlgebraInMathematicaOverview.html">Vectors and Matrices Mathematica</a>
+					</td>
+				</tr>
+				</table>
+			</div>
+		</div>
+	</main>
+	<footer class="page-footer">
       <div class="footer-copyright">
         <div class="container">
         © Jonathan Del Corpo
@@ -68,9 +68,9 @@
         </div>
       </div>
     </footer>
-		<!--Import jQuery before materialize.js-->
+	<!--Import jQuery before materialize.js-->
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-		<!-- Compiled and minified JavaScript -->
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
+	<!-- Compiled and minified JavaScript -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js"></script>
   </body>
 </html>
